854215d0df82ea555edf8923832a5bfcc00ad372
Soulant <845932620@qq.com>
2019-03-25 19:08:25 +0800

    Initial commit

---
+++ src/test/java/com/lion/LionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
