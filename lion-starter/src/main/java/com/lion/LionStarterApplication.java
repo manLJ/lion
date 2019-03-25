057b38ee4bfeb54ac81755cf6005c492a68dbca4
Soulant <845932620@qq.com>
2019-03-25 21:00:40 +0800

    pom 添加模块

---
+++ lion-starter/src/main/java/com/lion/LionStarterApplication.java
@@ -0,0 +1,13 @@
+package com.lion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionStarterApplication.class, args);
+    }
+
+}
